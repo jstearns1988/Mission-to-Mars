62baa2b04f126dbcbcdc42003aa9fe1bfc6846d1
jstearns1988 <101352424+jstearns1988@users.noreply.github.com>
2022-06-08 19:16:50 -0500

    Add files via upload
    ammended app.py file

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set": mars_data}, upsert=True)
-   return "Scraping Successful!"
+   return redirect('/', code=302)
 
 if __name__ == "__main__":
    app.run(debug=True)
